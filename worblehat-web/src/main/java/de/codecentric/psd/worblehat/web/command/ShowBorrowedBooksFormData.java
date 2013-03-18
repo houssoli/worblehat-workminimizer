401ef775932603efd0f27b74f6597f954c2c7a6d
Jan Lumbeck <jlumbeck@hotmail.de>
2013-03-18 13:43:12 +0100

    Test

--- worblehat-web/src/main/java/de/codecentric/psd/worblehat/web/command/ShowBorrowedBooksFormData.java
+++ worblehat-web/src/main/java/de/codecentric/psd/worblehat/web/command/ShowBorrowedBooksFormData.java
@@ -4,16 +4,16 @@ public class ShowBorrowedBooksFormData {
 
 	private String emailAdress;
 
+	public ShowBorrowedBooksFormData() {
+		super();
+	}
+
 	public String getEmail() {
 		return emailAdress;
 	}
 
 	public void setEmail(String email) {
 		this.emailAdress = email;
-	}
-
-	public ShowBorrowedBooksFormData() {
-		super();
 	}
 
 }
